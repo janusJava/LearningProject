b91db31c3b8bfb21bfa7cd4d61616c8967b175dd
Hubert Janus <janushubert0501@gmail.com>
2023-05-31 10:25:37 +0200

    Initial commit

---
+++ src/main/java/com/janus/learning/deeplearning/DeepLearningApplication.java
@@ -0,0 +1,13 @@
+package com.janus.learning.deeplearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janus/learning/deeplearning/DeepLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janus.learning.deeplearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
